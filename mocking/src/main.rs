eb24b66917cfd4df2c669e68bea4c64e635903f3
Benjamin Bara <benjamin.bara@skidata.com>
2025-03-10 17:01:38 +0100

    mocking: wrap existing code into test function

--- mocking/src/main.rs
+++ mocking/src/main.rs
@@ -9,9 +9,18 @@ fn call_with_four(x: &dyn MyTrait) -> u32 {
     x.foo(4)
 }
 
-let mut mock = MockMyTrait::new();
-mock.expect_foo()
-    .with(predicate::eq(4))
-    .times(1)
-    .returning(|x| x + 1);
-assert_eq!(5, call_with_four(&mock));
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_foo() {
+        let mut mock = MockMyTrait::new();
+        mock.expect_foo()
+            .with(predicate::eq(4))
+            .times(1)
+            .returning(|x| x + 1);
+        assert_eq!(5, call_with_four(&mock));
+    }
+}
